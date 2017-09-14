ca84ccdcdeb21ff08626ec2796282276bc421266
Kendra Moore <kendra.j.moore3443@gmail.com>
2017-09-14 16:17:17 -0700

    Giphy

---
+++ app.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+var http = require('http');
+var giphy = require('giphy-api')();
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  var queryString = req.query.term;
+  var term = encodeURIComponent(queryString);
+  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC';
+
+  http.get(url, function(response) {
+    response.setEncoding('utf8');
+
+    var body = '';
+
+    response.on('data', function(d) {
+      
+      body += d;
+    });
+
+    response.on('end', function() {
+
+      var parsed = JSON.parse(body);
+      res.render('home', {gifs: parsed.data})
+    });
+  });
+
+
+})
+
+app.get('/hello-gif', function (req, res) {
+  res.render('hello-gif', {url: 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'})
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
